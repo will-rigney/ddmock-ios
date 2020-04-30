32b9979b68b8490dd083ccfcf2d03ec4546fc3d8
Will Rigney <wrigney@deloitte.com.au>
2020-04-30 10:39:55 +1000

    add source files

---
+++ DDMockiOS/DDMockiOS.h
@@ -0,0 +1,19 @@
+//
+//  DDMockiOS.h
+//  DDMockiOS
+//
+//  Created by Bunduwongse, Natalie (AU - Sydney) on 18/3/19.
+//  Copyright © 2019 Bunduwongse, Natalie (AU - Sydney). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DDMockiOS.
+FOUNDATION_EXPORT double DDMockiOSVersionNumber;
+
+//! Project version string for DDMockiOS.
+FOUNDATION_EXPORT const unsigned char DDMockiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDMockiOS/PublicHeader.h>
+
+
---
+++ DDMockiOS/MockEntry.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct MockEntry: Codable {
+    internal static let defaultResponseTime = 400
+    internal static let defaultStatusCode = 200
+    
+    let path: String
+    var files = [String]()
+    var selectedFile = 0
+    private var statusCode = defaultStatusCode
+    var responseTime = defaultResponseTime
+    
+    init(path: String, files: [String]) {
+        self.path = path
+        self.files = files
+    }
+    
+    func getSelectedFile() -> Int {
+        return DDMockSettingsBundleHelper.getSelectedMockFile(key: path)
+    }
+    
+    func getStatusCode() -> Int {
+        return DDMockSettingsBundleHelper.getStatusCode(key: path)
+    }
+    
+    func useRealAPI() -> Bool {
+        return DDMockSettingsBundleHelper.useRealAPI(key: path)
+    }
+    
+    func getResponseTime() -> Int {
+        return DDMockSettingsBundleHelper.getResponseTime(key: path)
+    }
+}
