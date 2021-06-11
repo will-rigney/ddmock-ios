bd28963fcca0797ed24b41440e7e8d3827e7d4f7
Will Rigney <wrigney@deloitte.com.au>
2021-06-11 16:33:26 +1000

    move endpoint copy to function + additional clarity cleanup + constants

--- Generate/ddmock.py
+++ Generate/ddmock.py
@@ -9,11 +9,11 @@ import argparse
 import copy
 
 
+
 # todo: visibility
 
+
 # create the map of endpoints from mockfiles
-
-
 def generate_map(mockfiles_path):
     # init an empty map object
     endpoint_map = {}
@@ -22,8 +22,7 @@ def generate_map(mockfiles_path):
 
     # recursive directory traversal
     # todo: define subdir - it is the subdirectory configured (?)
-    for subdir, dirs, files in os.walk(mockfiles_path):
-        print(subdir)
+    for current, dirs, files in os.walk(mockfiles_path):
 
         # iterate through mockfiles
         for file in files:
@@ -31,14 +30,16 @@ def generate_map(mockfiles_path):
             if not file.endswith(".json"):
                 continue
 
+            # currently this only needs to look at files
+            # and create the key from the path
+            # should be a simpler api to use
+
             # todo: think there is a more normal way to check for only json files
 
             # this is to get the key from the json object path
-            # we can do this more directly
-            endpointPath = subdir.replace(mockfiles_path, "")
+            endpointPath = current.replace(mockfiles_path, "")
 
             # strip the leading slash if present
-            # todo: we should use a Path object instead
             if endpointPath.startswith("/"):
                 endpointPath = endpointPath.replace("/", "", 1)
 
@@ -55,6 +56,7 @@ def generate_map(mockfiles_path):
     return endpoint_map
 
 
+# open a file with the name res and return it
 def load_json_resource(res):
     print(f"{res}")
     with open(res, "r") as file:
@@ -62,7 +64,8 @@ def load_json_resource(res):
     return res
 
 
-def create_item(filename):
+# pure function returns an object to add to root preference specifier
+def create_root_item(filename):
     new_item = {}
     new_item['Type'] = 'PSChildPaneSpecifier'
     new_item['File'] = filename
@@ -78,6 +81,40 @@ def get_ddmock_path(resources_path):
     return path
 
 
+def create_endpoint_plist(endpoint, endpoint_path, filename, files):
+
+    # copy a new endpoint object
+    new_endpoint = copy.deepcopy(endpoint)
+
+    print(f"new endpoint: {new_endpoint}")
+
+    # replace variable keys in all the endpoint items
+
+    # for each item in preference specifiers list
+    for index, item in enumerate(new_endpoint["PreferenceSpecifiers"]):
+        # construct a new item
+        new_item = {}
+        # for every key value par in the item dict
+        for key, value in item.items():
+            try:
+                new_value = value.replace(
+                    "$endpointPathName", f"{endpoint_path}")
+                new_value = new_value.replace("$endpointPathKey", filename)
+                new_item[key] = new_value
+            except AttributeError:
+                # value can be any type
+                new_item[key] = value
+
+        new_endpoint["PreferenceSpecifiers"][index] = new_item
+
+    # set the mockfile "values" and "titles" fields
+    for setting in filter(lambda item: item['Title'] == "Mock file", new_endpoint['PreferenceSpecifiers']):
+        setting["Values"] = list(range(0, len(files)))
+        setting["Titles"] = files
+
+    return new_endpoint
+
+
 def main(mockfiles_path, output_path):
     path = get_ddmock_path("Resources")
     print(f"Template path: {path}")
@@ -90,11 +127,9 @@ def main(mockfiles_path, output_path):
     print(f"{endpoint_map}")
 
     # todo: better / dynamic configuration
-    # this is from invokation site, turns out
     settings_location = output_path
 
     # start creating settings bundle
-    # todo: what is the settings bundle & where are we creating it?
     print(f"Creating Settings.bundle at {settings_location}...")
 
     # Settings.bundle is really just a directory
@@ -108,52 +143,23 @@ def main(mockfiles_path, output_path):
     root = load_json_resource(path.joinpath("root.json"))
     endpoint = load_json_resource(path.joinpath("endpoint.json"))
 
-    # **
-    # what do we get out of this block? make it a function
-    # for path & files in map
+    # save each endpoint as a plist
     for endpoint_path, files in endpoint_map.items():
 
         print(f"Adding endpoint: {endpoint_path}")
 
-        # replaces the slashes with periods for
+        # replaces the slashes with periods for ...
         filename = endpoint_path.replace("/", ".")
 
         # add endpoint to root plist
-
-        new_item = create_item(filename)
+        new_item = create_root_item(filename)
         root['PreferenceSpecifiers'].append(new_item)
 
-        # create a copy of the endpoint plist replacing
-        # the $endpointPathName key     -> endpointPath
+        # create new endpoint object from endpoint template
+        new_endpoint = create_endpoint_plist(
+            endpoint, endpoint_path, filename, files)
 
-        # copy endpoint plist
-        new_endpoint = copy.deepcopy(endpoint)
-
-        print(f"ne: {new_endpoint}")
-
-        # replace keys in all the endpoint items
-
-        # for each item in preference specifiers list
-        for index, item in enumerate(new_endpoint["PreferenceSpecifiers"]):
-            # construct a new item
-            new_item = {}
-            # for every key value par in the item dict
-            for key, value in item.items():
-                try:
-                    new_value = value.replace("$endpointPathName", f"{endpoint_path}")
-                    new_value = new_value.replace("$endpointPathKey", filename)
-                    new_item[key] = new_value
-                except AttributeError:
-                    # value can be any type
-                    new_item[key] = value
-
-            new_endpoint["PreferenceSpecifiers"][index] = new_item
-
-        # set the mockfile "values" and "titles" fields
-        for setting in filter(lambda item: item['Title'] == "Mock file", new_endpoint['PreferenceSpecifiers']):
-            setting["Values"] = list(range(0, len(files)))
-            setting["Titles"] = files
-
+        # dump the endpoint to plist
         with open(settings_location + filename + ".plist", "wb") as fout:
             plistlib.dump(new_endpoint, fout, fmt=plistlib.FMT_XML)
 
---
+++ Sources/Constants.swift
@@ -0,0 +1,5 @@
+enum Constants {
+    // todo: make this more obvious or configurable
+    /// path under resources directory
+    static let mockDirectory = "/mockfiles"
+}
